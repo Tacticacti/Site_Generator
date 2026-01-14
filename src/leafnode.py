e1172bf43cb7b1251643e36f46ac88b1883ccaa3
Tacticacti <edwardoh@tudelft.nl>
2026-01-14 21:52:37 +0100

    Added more tests for leafnode

--- src/leafnode.py
+++ src/leafnode.py
@@ -9,7 +9,7 @@ class LeafNode(HTMLNode):
             raise ValueError("All LeafNode must have a value")
         if self.tag is None:
             return self.value
-        return f"<{self.tag}>{self.value}</{self.tag}>" if self.props is None else f"<{self.tag} {self.props}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>" if self.props is None else f"<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>"
 
     def __repr__(self):
-        return f"HTMLNode(tag={self.tag}, value={self.value}, props={self.props})"
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -4,6 +4,23 @@ from leafnode import LeafNode
 
 
 class TestLeafNode(unittest.TestCase):
-    def test_leaf_to_html_p(self):
+    def test_leaf_to_html_p_no_prop(self):
         node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.__repr__(),"LeafNode(tag=p, value=Hello, world!, props=None)")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p")
+        self.assertEqual(node.__repr__(),"LeafNode(tag=p, value=None, props=None)")
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(value="hello world!")
+        self.assertEqual(node.__repr__(),"LeafNode(tag=None, value=hello world!, props=None)")
+        self.assertEqual(node.to_html(), "hello world!")
+
+    def test_leaf_to_html_p_with_prop(self):
+        node = LeafNode("p", "Hello, world!", {"class": "greeting", "href": "https://boot.dev"})
+        self.assertEqual(node.__repr__(),"LeafNode(tag=p, value=Hello, world!, props={'class': 'greeting', 'href': 'https://boot.dev'})")
+        self.assertEqual(node.to_html(), '<p class="greeting" href="https://boot.dev">Hello, world!</p>')
