e673e2194713ca7fb071c1a584a31e2fb8cfc91a
Tacticacti <edwardoh@tudelft.nl>
2026-01-16 18:28:14 +0100

    Added parent node file

---
+++ src/parentnode.py
@@ -0,0 +1,16 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag in parent node!")
+        if self.children is None or len(self.children) == 0:
+            raise ValueError("No childern in parent node!")
+        childern_to_html = ""
+        for child in self.children:
+            childern_to_html += child.to_html()
+
+        return f"<{self.tag}>{childern_to_html}</{self.tag}>"
---
+++ src/test_parentnode.py
@@ -0,0 +1,55 @@
+import unittest
+from parentnode import ParentNode
+from leafnode import LeafNode
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_single_child(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_more_than_one_children(self):
+        child_node1 = LeafNode("span", "child1")
+        child_node2 = LeafNode("b", "child2")
+        parent_node = ParentNode("div", [child_node1,child_node2])
+        self.assertEqual(parent_node.to_html(), "<div><span>child1</span><b>child2</b></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_grandchildren_more_than_one_child(self):
+        child_node1 = LeafNode("span", "child1")
+        child_node2 = LeafNode("b", "child2")
+        parent_node1 = ParentNode("div", [child_node1,child_node2])
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node2 = ParentNode("div", [child_node])
+        parent_node = ParentNode("div", [parent_node1, parent_node2])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><div><span>child1</span><b>child2</b></div><div><span><b>grandchild</b></span></div></div>",
+        )
+
+    def test_to_html_no_tag(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode(children=[child_node])
+        with self.assertRaisesRegex(ValueError, "No tag in parent node!"):
+            parent_node.to_html()
+
+    def test_to_html_children_is_none(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode(tag="p")
+        with self.assertRaisesRegex(ValueError, "No childern in parent node!"):
+            parent_node.to_html()
+
+    def test_to_html_no_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode(tag="p", children=[])
+        with self.assertRaisesRegex(ValueError, "No childern in parent node!"):
+            parent_node.to_html()
