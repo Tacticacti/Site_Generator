a83ffd9181321edb2b81511bcbaaef2b30523c14
Tacticacti <edwardoh@tudelft.nl>
2026-01-17 15:34:52 +0100

    Wrote a function to change textnode to htmlnode

--- src/textnode.py
+++ src/textnode.py
@@ -23,14 +23,19 @@ class TextNode:
     def __repr__(self):
         return f"TextNode(text={self.text}, text_type={self.text_type}, url={self.url})"
 
-    def text_node_to_html_node(text_node):
-        match text_node.text_type:
-            case TextType.TEXT:
-                return LeafNode()
-            case TextType.BOLD:
-            case TextType.ITALIC:
-            case TextType.CODE:
-            case TextType.LINK:
-            case TextType.IMAGE:
-            
-        raise ValueError("Invalid text type!")
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url,"alt": text_node.text})
+        
+    raise ValueError(f"invalid text type: {text_node.text_type}")
